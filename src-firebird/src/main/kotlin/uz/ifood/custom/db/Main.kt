847eaa4bbb7c75d9d834e4b6bfb8b46e15ea7858
Bahrom Parpiyev <bahrom1982@gmail.com>
2024-10-31 21:22:27 +0500

    Commit

--- src-firebird/src/main/kotlin/uz/ifood/custom/db/Main.kt
+++ src-firebird/src/main/kotlin/uz/ifood/custom/db/Main.kt
@@ -1,4 +1,4 @@
-package uz.ifood.custom.db
+package uz.ifood.custom.db.uz.ifood.custom.db
 
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
